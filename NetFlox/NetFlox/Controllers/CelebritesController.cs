6cdc4d646206bd166b4fc6e4050504843b1da506
lrejou <lrejou@u-bordeaux.fr>
2019-02-13 20:02:24 +0100

    Projet .net

---
+++ NetFlox/NetFlox/Controllers/CelebritesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NetFlox.DAL;
+
+namespace NetFlox.Controllers
+{
+    public class CelebritesController : Controller
+    {
+        private readonly NetFloxEntities _context;
+
+        public CelebritesController(NetFloxEntities context)
+        {
+            _context = context;
+        }
+
+        // GET: Celebrites
+        public async Task<IActionResult> Index()
+        {
+            var celebrites = await _context.Celebrites
+                .OrderBy(c => c.Nom)
+                .ToListAsync();
+            return View(celebrites);
+        }
+
+        // GET: Celebrites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var celebrite = await _context.Celebrites
+                .Include(c => c.RoleCelebriteFilms)
+                .ThenInclude(rcf => rcf.Role)
+                .Include(c => c.RoleCelebriteFilms)
+                .ThenInclude(rcf => rcf.Film)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (celebrite == null)
+            {
+                return NotFound();
+            }
+
+            return View(celebrite);
+        }
+
+        public async Task<IActionResult> Search([FromQuery] String celeb)
+        {
+            var celebs = from m in _context.Celebrites
+                         select m;
+            if (!String.IsNullOrEmpty(celeb))
+            {
+                celebs = celebs.Where(s => EF.Functions.Like(s.Nom, "%" + celeb + "%"));
+            }
+            var nombre = celebs.Count();
+            ViewData["nb"] = nombre;
+            return View(await celebs.ToListAsync());
+        }
+    }
+}
---
+++ NetFlox/NetFlox/Models/Search.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetFlox.Models
+{
+    public class Search
+    {
+        public string champs { get; set; }
+        public string result { get; set; }
+    }
+}
